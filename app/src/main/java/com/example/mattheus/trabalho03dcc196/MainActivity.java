bdb591a304094b5722f4ed3b7f3fdbf914668396
Mattheus Soares <tz_santos@hotmail.com>
2018-12-05 16:41:45 -0200

    Mexendo na main actv

--- app/src/main/java/com/example/mattheus/trabalho03dcc196/MainActivity.java
+++ app/src/main/java/com/example/mattheus/trabalho03dcc196/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.mattheus.trabalho03dcc196;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn_cadastrar_aventura;
+    private RecyclerView recyclerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
