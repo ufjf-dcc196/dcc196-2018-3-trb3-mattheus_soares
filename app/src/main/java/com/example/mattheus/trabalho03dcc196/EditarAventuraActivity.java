d7936e061470566910fe45c564f9dca2acab68a2
Mattheus Soares <tz_santos@hotmail.com>
2018-12-09 00:14:38 -0200

    Começando a edição de aventura

--- app/src/main/java/com/example/mattheus/trabalho03dcc196/EditarAventuraActivity.java
+++ app/src/main/java/com/example/mattheus/trabalho03dcc196/EditarAventuraActivity.java
@@ -2,12 +2,18 @@ package com.example.mattheus.trabalho03dcc196;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class EditarAventuraActivity extends AppCompatActivity {
+    private Button btn_editar_aventura;
+    private TextView txt_nome, txt_desc, txt_forca, txt_destreza, txt_nervos, txt_const, txt_mente, txt_synth, txt_sabedoria, txt_carisma;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_aventura);
+
+
     }
 }
--- app/src/main/java/com/example/mattheus/trabalho03dcc196/MainActivity.java
+++ app/src/main/java/com/example/mattheus/trabalho03dcc196/MainActivity.java
@@ -1,17 +1,12 @@
 package com.example.mattheus.trabalho03dcc196;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private static ListarAventurasAdapter adapter;
